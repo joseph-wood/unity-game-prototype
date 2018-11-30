4217aaf4fc66147f089efbf2a3e3d2c3aeeed495
Joseph Wood <joseph.shaun.wood@gmail.com>
2018-11-30 14:58:26 +1000

    World menu added
    added scenes to build as well

---
+++ Mushbug/Assets/Scripts/OnClickLoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnClickLoadScene : MonoBehaviour {
+
+    public string levelName;
+
+    public void LoadScene(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+
+}
